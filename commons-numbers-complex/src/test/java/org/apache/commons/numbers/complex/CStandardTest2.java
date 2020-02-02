f4287699354f6804b67d8c1fad4c37db9077d330
Alex Herbert <aherbert@apache.org>
2020-02-02 23:47:36 +0000

    Switch hypot variant to Dekker split

--- commons-numbers-complex/src/test/java/org/apache/commons/numbers/complex/CStandardTest2.java
+++ commons-numbers-complex/src/test/java/org/apache/commons/numbers/complex/CStandardTest2.java
@@ -986,18 +986,18 @@ public class CStandardTest2 {
         // is slower again and is a poor trade-off to gain 1 ULP accuracy.
         final double w = x - y;
         if (w > y) {
-            final double t1 = Double.longBitsToDouble(Double.doubleToRawLongBits(x) & 0xffffffff00000000L);
-            //final double t1 = splitHigh(x);
+            //final double t1 = Double.longBitsToDouble(Double.doubleToRawLongBits(x) & 0xffffffff00000000L);
+            final double t1 = splitHigh(x);
             final double t2 = x - t1;
             return t1 * t1 - (y * (-y) - t2 * (x + t1));
         }
         // 2y > x > y
         final double t = x + x;
-        final double y1 = Double.longBitsToDouble(Double.doubleToRawLongBits(y) & 0xffffffff00000000L);
-        //final double y1 = splitHigh(y);
+        //final double y1 = Double.longBitsToDouble(Double.doubleToRawLongBits(y) & 0xffffffff00000000L);
+        final double y1 = splitHigh(y);
         final double y2 = y - y1;
-        final double t1 = Double.longBitsToDouble(Double.doubleToRawLongBits(t) & 0xffffffff00000000L);
-        //final double t1 = splitHigh(t);
+        //final double t1 = Double.longBitsToDouble(Double.doubleToRawLongBits(t) & 0xffffffff00000000L);
+        final double t1 = splitHigh(t);
         final double t2 = t - t1;
         return t1 * y1 - (w * (-w) - (t1 * y2 + t2 * y));
     }
